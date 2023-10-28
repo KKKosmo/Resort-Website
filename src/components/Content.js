0712a2e21c8e553cb1fed7fe84253edb10df98e3
manil <manilalouisangel2@gmail.com>
2023-10-29 00:02:29 +0800

    init commit

---
+++ src/components/Content.js
@@ -0,0 +1,8 @@
+function Content(){
+    return(
+        <div>
+            test
+        </div>
+    )
+}
+export default Content;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <div className="header">
+          <div className="row">
+                <a href="">
+                <i className="fa fa-facebook"></i>
+                </a>
+                <a href="tel:">
+                <i className="fa fa-phone"></i> 09090909
+                </a>
+                <a href="mailto:info@example.com">
+                <i className="fa fa-envelope"></i> email@gmail.com
+                </a>
+          </div>
+    </div>
+  );
+}
+
+export default Header;
