a9fcd79fc2a66a1d9795ed3d8eddac8189999412
manil <manilalouisangel2@gmail.com>
2023-10-30 03:10:10 +0800

    trying to get all css and images first

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
 function Header() {
   return (
-    <div className="header">
-          <div className="row">
-                <a href="">
-                <i className="fa fa-facebook"></i>
-                </a>
-                <a href="tel:">
-                <i className="fa fa-phone"></i> 09090909
-                </a>
-                <a href="mailto:info@example.com">
-                <i className="fa fa-envelope"></i> email@gmail.com
-                </a>
-          </div>
+    <div class="header">
+        <div class="header-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-sm-6 col-12">
+                        <ul class="header-social">
+                            <li class="facebook"><a href=""><i class="fa fa-facebook"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6 col-sm-6 col-12">
+                        <ul class="header-info">
+                            <li><a href="tel:%20+33%20555%2066%20777"><i class="fa fa-phone"></i> 09090909</a></li>
+                            <li><a href="mailto:info@example.com"><i class="fa fa-envelope"></i> email@gmail.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="nav-spacer"></div>
+        <div class="clearfix"></div>
     </div>
   );
 }
